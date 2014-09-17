1f932b65e981eee962a49d0b08ef6b9863759141
Eden Li <eden@realtime.tw>
2014-09-17 09:29:45 +0800

    Initial Commit

---
+++ HealthKit~Swift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HealthKit~Swift
+//
+//  Created by EdenLi on 2014/9/17.
+//  Copyright (c) 2014年 Darktt Personal Company. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
